c9badba3a4597998b3a5394cdb50a50873e733a5
Brendan O'Fallon <brendan@thechampagneofdesktops>
2011-07-31 10:16:09 -0700

    Tweaks for UI and examples, added new SpeedMonitor monitor type

---
+++ src/gui/SpeedMonitor.java
@@ -0,0 +1,46 @@
+package gui;
+
+import java.awt.geom.Point2D;
+
+import gui.figure.series.XYSeries;
+import gui.figure.series.XYSeriesElement;
+
+/**
+ * A simple monitor that displays the "speed" (mc states / sec) of the chains
+ * @author brendan
+ *
+ */
+public class SpeedMonitor extends MonitorPanel {
+
+	private long lastTime;
+	private int lastState = 0;
+	
+	public SpeedMonitor() {
+		series = new XYSeries[1];
+		series[0] = new XYSeries("mc.rate");
+		titles = new String[1];
+		titles[0] = "Log likelihood";
+		XYSeriesElement serEl = traceFigure.addDataSeries(series[0]);
+		serEl.setLineWidth(defaultLineWidth);
+		lastTime = System.currentTimeMillis();
+	}
+	
+	@Override
+	protected void update(int state) {
+		long timeNow = System.currentTimeMillis();
+		double speed = (state - lastState)/(timeNow-lastTime)/1000.0 ;
+		lastState = state;
+		lastTime = timeNow;
+		
+		Point2D.Double point = new Point2D.Double(state, speed);
+		series[0].addPointInOrder(point);
+
+		if (histoSeries != null) {
+			createHistograms();
+			histoSeries[0].addValue(speed);
+		}
+		traceFigure.inferBoundsPolitely();
+		repaint();
+	}
+
+}
