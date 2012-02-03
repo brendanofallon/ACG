2ebeecf5f1623d891b39e01f72af0eb1e0e293c8
Brendan <brendanofallon@fastmail.fm>
2012-02-03 14:06:54 -0700

    Minor changes from work machine

--- src/tools/ARGUtils.java
+++ src/tools/ARGUtils.java
@@ -187,8 +187,8 @@ public class ARGUtils {
 	
 	public static String getHelpString() {
 		StringBuilder str = new StringBuilder();
-		str.append("ARGUtils v0.01 : Some utilities read extracting information from ARG text files \n");
-		str.append(" Author: Brendan O'Fallon (brendano@u.washington.edu) \n");
+		str.append("ARGUtils v0.01 : Some utilities for extracting information from ARG text files \n");
+		str.append(" Author: Brendan O'Fallon (brendanofallon@fastmail.fm) \n");
 		str.append("  For now, all ARGs must be in GraphML-like format \n");
 		str.append("  usage \n");
 		str.append(" --extract-trees [argfile.xml]         \n\t Emit all marginal trees in newick format \n");
@@ -307,6 +307,7 @@ public class ARGUtils {
 				List<RecombNode> rNodes = arg.getRecombNodes();
 				System.out.println("Number of breakpoints : " + rNodes.size());
 				System.out.println("Site \t Height");
+				Collections.sort(rNodes, arg.getBPPosComparator());
 				for(RecombNode node : rNodes) {
 					System.out.println(node.getInteriorBP() + "\t" + node.getHeight());
 				}
